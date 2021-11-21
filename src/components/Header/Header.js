1953b95205cf99e71dcc6f91532ee78e15b87000
Daniel C <azenis.ofc@gmail.com>
2021-11-21 10:44:32 +0100

    exercise 5

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -60,7 +60,6 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
 
   @media ${QUERIES.tabletAndDown} {
@@ -77,7 +76,7 @@ const MainHeader = styled.div`
 
 const Nav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1.5rem, 8vw - 4rem, 3rem);
   margin: 0px 48px;
 
   @media ${QUERIES.tabletAndDown} {
